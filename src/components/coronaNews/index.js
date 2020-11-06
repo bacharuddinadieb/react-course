731e53d1305e4f7bf761d2ccae6435ae071a1cec
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-11-06 14:26:24 +0700

    menampilkan news dengan card

--- src/components/coronaNews/index.js
+++ src/components/coronaNews/index.js
@@ -4,6 +4,13 @@ import Cards from '../cards';
 import 'firebase/database';
 import './style.css';
 
+const convertTanggalKeLocale = (tanggal) => {
+  const tanggalLokal = new Date(tanggal).toLocaleString('id-ID', {
+    dateStyle: 'full',
+  });
+  return tanggalLokal;
+};
+
 const CoronaNews = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -19,28 +26,33 @@ const CoronaNews = () => {
   }, []);
 
   console.log(news);
-
   return (
     <div>
       <h2>data corona</h2>
       {isLoading ? <p>loading</p> : <p>data</p>}
       {/* <p>{JSON.stringify(news)}</p> */}
-      <div className="news-wrapper corona__news-wrapper">
-        <Cards>
-          <h1>wuuuh</h1>
-        </Cards>
-        <Cards>
-          <h1>wuuuh</h1>
-        </Cards>
-        <Cards>
-          <h1>wuuuh</h1>
-        </Cards>
-        <Cards>
-          <h1>wuuuh</h1>
-        </Cards>
-        <Cards>
-          <h1>wuuuh</h1>
-        </Cards>
+      <div className="corona__news-wrapper">
+        {news.map((newsItem) => {
+          return (
+            <div>
+              <h1 style={{ marginTop: '2em' }}>
+                {convertTanggalKeLocale(newsItem.date)}
+              </h1>
+              <div className="news-wrapper">
+                {newsItem.activity.map((activityItem) => {
+                  return (
+                    <a href={activityItem.url} target="_blank" rel="noreferrer">
+                      <Cards>
+                        <h2 className="news-title">{activityItem.title}</h2>
+                        <p className="news-description">{activityItem.desc}</p>
+                      </Cards>
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
