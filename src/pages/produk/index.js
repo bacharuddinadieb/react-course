1e2f0a95e26f51b758d50acc693126a730818c0d
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-12-04 14:41:42 +0700

    penambahan halaman produk dan menampilkan produk

--- src/pages/produk/index.js
+++ src/pages/produk/index.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { authService } from '../../services';
+import Cards from '../../components/cards/index';
+import './style.css';
 
 const Produk = () => {
-  return <div> Produk Page!</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataProduk, setDataProduk] = useState([]);
+
+  const getProduk = () => {
+    authService
+      .getProduk()
+      .then((res) => {
+        console.log(res);
+        setDataProduk(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getProduk();
+  }, []);
+
+  return (
+    <div>
+      <h1>Produk Page!</h1>
+      <div className="produk-wrapper">
+        {dataProduk.map((produk) => {
+          return (
+            <Cards style={{ fontSize: 18, lineHeight: 1.5 }}>
+              <h1>{produk.description}</h1>
+              <div className="diskon-wrapper">
+                <div className="diskon-wrapper__percentage">
+                  {produk.display_promo_price_percentage}
+                </div>
+                <span className="harga-produk">
+                  {produk.display_normal_price}
+                </span>
+              </div>
+              <h3 className="harga-produk-terdiskon">{produk.display_price}</h3>
+              <p>{produk.name}</p>
+            </Cards>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Produk;
